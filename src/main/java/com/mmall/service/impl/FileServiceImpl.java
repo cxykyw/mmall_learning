8f38865a60458f4336a81b03d7d6be40709f6b97
kangyuwei <1337441734@qq.com>
2018-09-03 08:58:06 +0800

    9-3commit

---
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -0,0 +1,53 @@
+package com.mmall.service.impl;
+
+import com.google.common.collect.Lists;
+import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service("iFileService")
+public class FileServiceImpl implements IFileService {
+
+    private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
+    public String upload(MultipartFile file,String path){
+        String fileName = file.getOriginalFilename();
+        //扩展名
+        String fileExtendsionName = fileName.substring(fileName.lastIndexOf(".")+1);
+        String uploadFileName = UUID.randomUUID().toString()+"."+fileExtendsionName;
+        logger.info("开始上传文件,上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName,path,uploadFileName);
+
+        File fileDir = new File(path);
+        if(!fileDir.exists()){
+            fileDir.setWritable(true);
+            fileDir.mkdirs();
+        }
+
+        File targetFile = new File(path,uploadFileName);
+
+        try {
+            file.transferTo(targetFile);
+            //至此文件上传成功
+            FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+
+            targetFile.delete();
+
+        } catch (IOException e) {
+            logger.error("上传文件异常",e);
+            return null;
+        }
+        return targetFile.getName();
+    }
+
+   /* public static void main(String[] args) {
+        String fileName = "abc.jpg";
+        System.out.println(fileName.substring(fileName.lastIndexOf(".")+1));
+    }*/
+}
