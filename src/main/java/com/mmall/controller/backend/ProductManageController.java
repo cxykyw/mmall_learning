0063b74a212806e7d7cbf527d2136edd0a30be1f
kangyuwei <1337441734@qq.com>
2018-09-04 19:08:25 +0800

    9-4commit

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -130,7 +131,7 @@ public class ProductManageController {
         if(iUserService.checkAdminRole(user).isSuccess()){
             String path = request.getSession().getServletContext().getRealPath("upload");
             String targetFileName = iFileService.upload(file,path);
-            String url = PropertiesUtil.getProperty("ftp.server.http.pref")+targetFileName;
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
 
             Map fileMap = Maps.newHashMap();
             fileMap.put("uri",targetFileName);
@@ -138,6 +139,43 @@ public class ProductManageController {
             return ServerResponse.createBySuccess(fileMap);
         }else{
             return ServerResponse.createByErrorMessage("无权限操作");
+        }
+
+    }
+
+
+    @RequestMapping("richtext_img_upload.do")
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, @RequestParam(value = "upload_file",required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response){
+        Map resultMap = Maps.newHashMap();
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            resultMap.put("success",false);
+            resultMap.put("msg","请登录管理员");
+            return resultMap;
+        }
+
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file,path);
+
+            if(StringUtils.isBlank(targetFileName)){
+                resultMap.put("success",false);
+                resultMap.put("msg","上传失败");
+                return resultMap;
+            }
+
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix")+targetFileName;
+            resultMap.put("success",true);
+            resultMap.put("msg","上传成功");
+            resultMap.put("file_path",url);
+            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            return resultMap;
+
+        }else{
+            resultMap.put("success",false);
+            resultMap.put("msg","无权限操作");
+            return resultMap;
         }
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,11 +2,18 @@
 <body>
 <h2>Hello World!</h2>
 
-springmvc 上传文件
+springmvc uploadfile
 <form name="" action="/manage/product/upload.do" method="post" enctype="multipart/form-data">
 
     <input type="file" name="upload_file">
-    <input type="submit" value="上传文件">
+    <input type="submit" value="upload">
+</form>
+
+richtext uploadfile
+<form name="" action="/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
+
+    <input type="file" name="upload_file">
+    <input type="submit" value="upload">
 </form>
 
 </body>
