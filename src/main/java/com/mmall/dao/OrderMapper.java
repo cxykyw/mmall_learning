9fd4d17e2f34a7ba9aaaf928197db7e9de9c80fe
kangyuwei <1337441734@qq.com>
2018-09-13 09:24:22 +0800

    9-13commit

--- src/main/java/com/mmall/dao/OrderMapper.java
+++ src/main/java/com/mmall/dao/OrderMapper.java
@@ -1,6 +1,8 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Order;
+import com.mmall.pojo.OrderItem;
+import org.apache.ibatis.annotations.Param;
 
 public interface OrderMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +16,8 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Order selectByUserIdOrderNo(@Param("userId") Integer userId, @Param("orderNo")Long orderNo);
+
+
 }
